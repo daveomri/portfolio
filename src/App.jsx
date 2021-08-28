1944b32ce2fc732c147d05f4651a5ef50320144e
Dave Omrai <daom9999@gmail.com>
2021-08-29 00:37:07 +0200

    Changing the layout a bit, and adding possible ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Paper } from '@material-ui/core';
+import { Paper, Box } from '@material-ui/core';
 
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 const useStyles = makeStyles(theme => ({
   root: {
     margin: 'auto',
     display: 'block',
     backgroundColor: '#e55',
-    height: '100vh',
     [theme.breakpoints.up('xs')]: {
       width: '75vw',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '50vw',
     },
     [theme.breakpoints.down('xs')]: {
       width: '100vw',
     },
+  },
+  content: {
+    height: '100vh',
+    backgroundColor: '#462f',
   },
 }));
 
@@ -26,6 +33,8 @@ const App = () => {
   return (
     <Paper className={classes.root}>
       <Header classes={classes.header} />
+      <Box className={classes.content}>content</Box>
+      <Footer classes={classes.footer} />
     </Paper>
   );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({}));
-
-const Header = () => {
-  const classes = useStyles();
+const Header = props => {
+  const { classes } = props;
 
   return (
-    <Grid container className={classes}>
+    <Grid container className={classes.root}>
       <Grid item xs={6} md={4}>
-        First item
+        <Grid container>
+          <Grid item xs={12}>
+            logo
+          </Grid>
+          <Grid item xs={12}>
+            some info
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs={6} md={4}>
-        Second item
+        <Grid container>
+          <Grid item xs={12}>
+            name
+          </Grid>
+          <Grid item xs={12}>
+            some info
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs={6} md={4}>
-        Third item
+        <Grid container>
+          <Grid item xs={12}>
+            name
+          </Grid>
+          <Grid item xs={12}>
+            some info
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container>
+          <Grid>my name and who created it</Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
+};
+
+Header.displayName = 'Header';
+
+Header.propTypes = {
+  classes: PropTypes.object,
+};
+
+Header.defaultProps = {
+  classes: {},
 };
 
 export default Header;
