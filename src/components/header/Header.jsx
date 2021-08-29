fb5e8622f67edcdb0dace02c2987629700a39710
Dave Omrai <daom9999@gmail.com>
2021-08-29 15:42:05 +0200

    Fixed the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Grid, Button } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  IconButton,
+  List,
+  SwipeableDrawer,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
+
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles(theme => ({
   button: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
   },
+  drawer: {
+    width: 'auto',
+    padding: theme.spacing(2),
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const [state, setState] = useState(false);
+
+  const handleState = newState => () => {
+    setState(newState);
+  };
+
+  const list = () => (
+    <div
+      // className={clsx(classes.list, {
+      //   [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      // })}
+      role="presentation"
+      onClick={handleState(false)}
+      onKeyDown={handleState(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Grid container alignItems="stretch">
+        <Grid container alignItems="center">
           <Grid item xs={3} sm={2} md={1}>
             <Typography variant="h6" className={classes.title}>
               Portfolio
@@ -23,33 +88,22 @@ const Header = () => {
           </Grid>
           <Grid item xs={9} sm={10} md={11}>
             <Grid container justifyContent="flex-end">
-              <Grid item xs={4} sm={2}>
-                <Button className={classes.button} fullWidth>
-                  one
-                </Button>
-              </Grid>
-              <Grid item xs={4} sm={2}>
-                <Button className={classes.button} fullWidth>
-                  two
-                </Button>
-              </Grid>
-              <Grid item xs={4} sm={2}>
-                <Button className={classes.button} fullWidth>
-                  three
-                </Button>
-              </Grid>
-              {/* <Grid item xs={3} md={2}>
-                <Button className={classes.button} fullWidth>menu</Button>
-                {/ * <SwipeableDrawer
-                  anchor="right"
-                  onOpen={handleState(true)}
-                  open={state}
-                  onClose={handleState(false)}
-                  className={classes.drawer}
-                >
-                  Drawer
-                </SwipeableDrawer> * /}
-              </Grid> */}
+              <IconButton
+                className={classes.button}
+                aria-label="menu"
+                onClick={handleState(true)}
+              >
+                <Menu />
+              </IconButton>
+              <SwipeableDrawer
+                anchor="right"
+                onOpen={handleState(true)}
+                open={state}
+                onClose={handleState(false)}
+                classes={{ paper: classes.drawer }}
+              >
+                {list()}
+              </SwipeableDrawer>
             </Grid>
           </Grid>
         </Grid>
