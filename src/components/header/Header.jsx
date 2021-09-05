0e54f4858cd6c845968a456b0077fbc4f833dfa7
David Omrai <david.omrai@seznam.cz>
2021-09-05 23:16:20 +0200

    Changing the let to const

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -87,7 +87,7 @@ const Header = () => {
   const classes = useStyles();
   const [state, setState] = useState(false);
 
-  let pathName = window.location.pathname.split('/').slice(-1).pop();
+  const pathName = window.location.pathname.split('/').slice(-1).pop();
   const pathKey = pathToKey[`/${pathName}`];
 
   const handleState = (newState) => () => {
