3fe6e7cf882c6b48992f73d0b10f3288f653f1ef
David Omrai <david.omrai@seznam.cz>
2021-09-10 01:05:18 +0200

    #5 Added buttons for links

--- src/Links.jsx
+++ src/Links.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Link } from '@material-ui/core';
+import { Box, Grid, Button } from '@material-ui/core';
 
 import links from './links';
 import profile from './resource/images/profile.png';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   content: {
     minHeight: '100vh',
+    paddingTop: '10vh',
+    paddingBottom: '10vh',
   },
   profilePic: {
     minWidth: '150px',
@@ -16,33 +18,53 @@ const useStyles = makeStyles(() => ({
     height: 'auto',
     border: '1px solid black',
   },
+  link: {
+    marginTop: theme.spacing(1),
+  },
 }));
 
 const Links = () => {
   const classes = useStyles();
 
   return (
-    <Box>
-      <Grid container justifyContent="center" alignItems="center" className={classes.content}>
-        <Grid item xs={12}>
+    <Box className={classes.content}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={8} md={4}>
           <Grid container direction="column" spacing={2}>
             <Grid item xs={12}>
-              <Grid container direction="column" justifyContent="center" alignItems="center">
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
                 <img src={profile} alt="Logo" className={classes.profilePic} />
               </Grid>
             </Grid>
-            {links.map((linkSection) => (
-              <Grid item xs={12}>
-                <Grid container direction="column" justifyContent="center" alignItems="center">
+            {links.map((linkSection, key) => (
+              <Grid item xs={12} key={key}>
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={1}
+                >
                   <Grid item xs={12}>
                     {linkSection.name}
                   </Grid>
-                  {linkSection.links.map((link) => (
-                    <Grid item>
-                      <Link href={link.url}>
-                        {link.name}
-                      </Link>
-                    </Grid>
+                  {linkSection.links.map((link, keyy) => (
+                    <Button
+                      fullWidth
+                      key={keyy}
+                      variant="outlined"
+                      color="primary"
+                      className={classes.link}
+                      target="_blank"
+                      href={link.url}
+                    >
+                      {link.name}
+                    </Button>
                   ))}
                 </Grid>
               </Grid>
